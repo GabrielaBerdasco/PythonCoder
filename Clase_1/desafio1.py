14a03d534a716d693efa1f24256a46024bf00e81
Gabriela.Berdasco <gabriela_berdasco@hotmail.com>
2022-06-22 21:37:33 -0300

    First commit

---
+++ Clase_1/desafio1.py
@@ -0,0 +1,9 @@
+nota_1 = int(input('1era nota: '))
+nota_2 = int(input('2da nota: '))
+
+nota_final = 0.4*nota_1 + 0.6*nota_2
+
+texto = "La nota final es: "
+
+print(texto)
+print(nota_final)
---
+++ Clase_1/desafio_generico.py
@@ -0,0 +1,22 @@
+cadena_1 = 'moderno'
+cadena_2 = "Python"
+cadena_3 = 'es un lenguaje'
+cadena_4 = 'de programación'
+
+print(cadena_2 +' '+ cadena_3 + ' ' + cadena_4 + ' ' + cadena_1)
+
+
+cadena = "acitametaM ,5.8 ,otipeP ordeP"
+cadena_volteada = cadena[::-1]
+
+print(cadena_volteada)
+
+
+nombre_alumno = cadena_volteada[0:12]
+print(nombre_alumno)
+
+nota = cadena_volteada[14:17]
+print(nota)
+
+materia = cadena_volteada[19:29]
+print(materia)
